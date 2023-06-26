70e24cb74fe71b8473fd0643951d63f9f2591088
user530 <m.timarsuev@gmail.com>
2023-06-26 15:36:24 +0300

    Disabled vlasky mysql

--- server/main.ts
+++ server/main.ts
@@ -1,12 +1,12 @@
 import { Meteor } from 'meteor/meteor';
-import { LiveMysql } from "meteor/vlasky:mysql";
+// import { LiveMysql } from "meteor/vlasky:mysql";
 import { callMyMethod, registerMyMethods } from '/imports/api/helpers';
 import { mysqlConnectionSettings } from '/imports/api/types';
 
 Meteor.startup(async () => {
   // Establish DB config using Vlasky:mysql using settings from the file
   const dbConfig: mysqlConnectionSettings = Meteor.settings.mysql;
-  const liveDb: LiveMysql = new LiveMysql(dbConfig);
+  const liveDb = new LiveMysql(dbConfig);
 
   // Publish users data for meteor subscription
   Meteor.publish('allCustomers', function () {
@@ -16,7 +16,7 @@ Meteor.startup(async () => {
       INNER JOIN positions AS p 
       ON c.position_id = p.id ORDER BY c.id`,
       null,
-      LiveMysql.LiveMysqlKeySelector.Index(),
+      LiveMysqlKeySelector.Index(),
       [{ table: 'customers' }, { table: 'positions' }]
     )
   })
